c2ef97360e79c24f3a443d0a5484b53cde2e2a03
Theoxylo <theoxylo@yahoo.com>
2012-10-16 14:41:13 -0700

    added function processCinFromOtherDP

--- test/test.js
+++ test/test.js
@@ -10,38 +10,9 @@ if (process.argv.length !== 3) throw 'Usage: node path/test.js path/test.xml'
 var file = process.argv.length == 3 ? process.argv.pop() : '';
 log('file arg: ' + file);
 
-gdsn.getInstanceIdFromFile(
-    file,
-    function(err, id) {
-        if (err) {
-            throw(err);
-        }
-        console.log('doc instance id: ' + id);
-    }
-);
-
-
-log("Changing document instance id and saving to new file...");
-gdsn.readXmlFile(
-    file,
-    function (err, xml) { // cb
-        if (err) {
-            throw err;
-        }
-        log('original xml length: ' + Buffer.byteLength(xml));
-        
-        var oldId = gdsn.getInstanceId(xml);
-        log("old id from original xml: " + oldId);
-        
-        var modXml = gdsn.updateInstanceId(xml, oldId + '_MOD');
-        log('modified xml length: ' + Buffer.byteLength(modXml));
-        
-        var newId = gdsn.getInstanceId(modXml);
-        log("new id from modified xml: " + newId);
-        //log("modified xml: " + modXml);
-        
-        gdsn.writeXmlFile(file + '-MOD', modXml);
-    }
-);
-
-
+log("Transforming CIN from other data pool for local trading party recipient...");
+gdsn.readXmlFile( file, function (err, xml) {
+    if (err) throw err;
+    var modXml = gdsn.processCinFromOtherDP(xml);
+    gdsn.writeXmlFile(file + '-to_local_party.xml', modXml);
+});

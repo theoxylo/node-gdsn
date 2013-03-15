b95f2f4dddb764e0c7b11361fb580fcc793eb116
theoxylo <theoxylo@yahoo.com>
2013-03-15 16:27:37 -0700

    util for responding to a list of files, eg using xargs

---
+++ cin_respond.js
@@ -0,0 +1,43 @@
+(function () {
+  
+  var Gdsn = require('./index.js')
+  
+  var gdsn = new Gdsn({
+    homeDataPoolGln: '1100001011285'
+  })
+  
+  console.log(gdsn.getVersion())
+
+  if (process.argv.length < 3) {
+    console.log("usage: node script file")
+    process.exit(1)
+  }
+
+  var processFile = function(file) {
+
+    console.log('file arg: ' + file)
+    
+    gdsn.readXmlFile(file, function(err, xml) {
+      if (err) {
+        console.log('Error: ' + err)
+        process.exit(1)
+      }
+      var doc = gdsn.getDocForXml(xml)
+      gdsn.createCinResponse(doc, function(err, responseXml) {
+        var outputFile = file + '_response'
+        gdsn.writeXmlFile(outputFile, responseXml, function(err, result) {
+          if (err) {
+            console.log('Error: ' + err)
+            process.exit(1)
+          }
+          console.log('Created new CIN response file: ' + outputFile)
+        })
+      })
+    })
+  }
+
+  while (process.argv.length > 2) {
+    processFile(process.argv.pop())
+  }
+  
+}())

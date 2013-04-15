5ccd61d62c8385cb32d56be329902233db9805a9
theoxylo <theoxylo@yahoo.com>
2013-04-15 12:19:00 -0700

    all functions now async, no returns

--- index.js
+++ index.js
@@ -2,50 +2,52 @@ var select = require('xpath.js')
 var xmldom = require('xmldom')
 var fs = require('fs')
 
-module.exports = function Gdsn(opts) {
+module.exports = Gdsn
 
+function Gdsn(opts) {
   if (!(this instanceof Gdsn)) return new Gdsn(opts)
 
-  this.opts = opts || {} // e.g. { homeDataPoolGln: '1100001011285', templatePath: './node_modules/gdsn/templates/' }
+  this.xmldom_parser = new xmldom.DOMParser()
+  this.xmldom_serializer = new xmldom.XMLSerializer()
 
-  var homeDataPoolGln = opts.homeDataPoolGln
+  opts = opts || {} 
+  if (!opts.templatePath) opts.templatePath = __dirname + '/templates/'
+  if (!opts.homeDataPoolGln) opts.homeDataPoolGln = '1100001011285'
 
-  this.getDocForXml = function(xml) {
-    return new xmldom.DOMParser().parseFromString(xml)
+  this.getXmlDom = function(xml, cb) {
+    var doc = this.xmldom_parser.parseFromString(xml, "text/xml")
+    cb(null, doc)
+  }
+
+  this.handleErr = function(err, cb) {
+    if (err) {
+      cb(err)
+      return true
+    }
+    return false
   }
   
   this.readXmlFile = function(file, cb) {
     var log = function(msg) {
       console.log('gdsn.readXmlFile: ' + msg)
-    };
-    fs.readFile(
-    file, 'utf-8',
-    function(err, xml) { // cb
-      if (err) {
-        cb(err)
-      }
+    }
+    var self = this
+    fs.readFile(file, 'utf-8', function (err, xml) {
+      if (self.handleErr(err, cb)) return
       log('file byte count: ' + Buffer.byteLength(xml))
       cb(null, xml)
-    });
+    })
   }
 
   this.writeXmlFile = function(file, xml, cb) {
     var log = function(msg) {
       console.log('gdsn.writeXmlFile: ' + msg)
     }
-    fs.writeFile(
-      file,
-      xml, 'utf-8',
-      function(err, xml) { // cb
-        if (err) {
-          if (!cb) throw err
-          cb(err)
-          return
-        }
-        if (cb) cb(null, 'File saved: ' + file)
-        else log('File saved: ' + file)
-      }
-    )
+    var self = this
+    fs.writeFile(file, xml, 'utf-8', function (err, xml) {
+      if (self.handleErr(err, cb)) return
+      cb(null, 'File saved: ' + file)
+    })
   }
   
   this.getMessageInfo = function(doc) {
@@ -76,49 +78,47 @@ module.exports = function Gdsn(opts) {
       console.log('gdsn.createCinResponse: ' + msg)
     };
     
-    var self = this;
     var msgIn = this.getMessageInfo(doc);
     log("inbound msg info: ");
     console.log(msgIn);
     
-    this.readXmlFile(this.opts.templatePath + "GDSNResponse_template.xml", function (err, responseTemplate) {
-      if (err) {
-        throw err;
-      }
-      var res = new xmldom.DOMParser().parseFromString(responseTemplate)
-      self.populateResponseTemplate(res, msgIn)
-      
-      var message = res.getElementsByTagName("eanucc:message")[0]
-      
-      var eANUCCResponse = res.createElement("gdsn:eANUCCResponse")
-      eANUCCResponse.setAttribute("responseStatus", "ACCEPTED")
-      var sender = res.createElement("sender")
-      sender.appendChild(res.createTextNode(msgIn.sender))
-      eANUCCResponse.appendChild(sender)
-      var receiver = res.createElement("receiver")
-      receiver.appendChild(res.createTextNode(msgIn.receiver))
-      eANUCCResponse.appendChild(receiver)
+    var self = this;
+    this.readXmlFile(opts.templatePath + "GDSNResponse_template.xml", function (err, responseTemplate) {
+      if (self.handleErr(err, cb)) return
+      self.getXmlDom(responseTemplate, function (err, res) {
+        self.populateResponseTemplate(res, msgIn)
+        
+        var message = res.getElementsByTagName("eanucc:message")[0]
+        
+        var eANUCCResponse = res.createElement("gdsn:eANUCCResponse")
+        eANUCCResponse.setAttribute("responseStatus", "ACCEPTED")
+        var sender = res.createElement("sender")
+        sender.appendChild(res.createTextNode(msgIn.sender))
+        eANUCCResponse.appendChild(sender)
+        var receiver = res.createElement("receiver")
+        receiver.appendChild(res.createTextNode(msgIn.receiver))
+        eANUCCResponse.appendChild(receiver)
 
-      var nodes = select(doc, "//*[local-name() = 'transaction']/entityIdentification")
-      
-      var i, j, responseNode, documentReceived
-      for (i = 0; i < nodes.length; i++) {
-        responseNode = eANUCCResponse.cloneNode(true)
-        documentReceived = res.createElement("documentReceived")
-        for (j = 0; j < nodes[i].childNodes.length; j++) {
-          if (nodes[i].childNodes[j].nodeType === 1) {
-            documentReceived.appendChild(nodes[i].childNodes[j].cloneNode(true))
+        var nodes = select(doc, "//*[local-name() = 'transaction']/entityIdentification")
+        
+        var i, j, responseNode, documentReceived
+        for (i = 0; i < nodes.length; i++) {
+          responseNode = eANUCCResponse.cloneNode(true)
+          documentReceived = res.createElement("documentReceived")
+          for (j = 0; j < nodes[i].childNodes.length; j++) {
+            if (nodes[i].childNodes[j].nodeType === 1) {
+              documentReceived.appendChild(nodes[i].childNodes[j].cloneNode(true))
+            }
           }
+          responseNode.appendChild(documentReceived)
+          message.appendChild(responseNode)
         }
-        responseNode.appendChild(documentReceived)
-        message.appendChild(responseNode)
-      }
-      
-      var resXml = new xmldom.XMLSerializer().serializeToString(res)
-      log("Response xml length: " + resXml.length)
-      
-      if (cb) cb(null, resXml)
-      else return resXml
+        
+        var resXml = new xmldom.XMLSerializer().serializeToString(res)
+        log("Response xml length: " + resXml.length)
+        
+        cb(null, resXml)
+      }) // end getXmlDom
     })
   }
 
@@ -130,11 +130,12 @@ module.exports = function Gdsn(opts) {
     var info = this.getMessageInfo(doc)
 
     // check that receiver is the home data pool:
-    if (info.receiver !== homeDataPoolGln) {
-      throw {
+    if (info.receiver !== opts.homeDataPoolGln) {
+      cb({
         name: 'processCinFromOtherDP',
-        message: 'message must be addressed to home data pool GLN ' + homeDataPoolGln
-      }
+        message: 'message must be addressed to home data pool GLN ' + opts.homeDataPoolGln
+      })
+      return
     }
     // set sender to home data pool
     select(doc, "//*[local-name() = 'Sender']/*[local-name() = 'Identifier']")[0].firstChild.data = info.receiver
@@ -142,11 +143,12 @@ module.exports = function Gdsn(opts) {
     // get data recipient (same for all transactions) and set new receiver gln
     var dataRecipient = select(doc, "//catalogueItem/dataRecipient")[0].firstChild.data
     log("CatalogueItem.dataRecipient GLN: " + dataRecipient)
-    if (dataRecipient === homeDataPoolGln) {
-      throw {
+    if (dataRecipient === opts.homeDataPoolGln) {
+      cb({
         name: 'processCinFromOtherDP',
         message: 'dataRecipient must be a local party, not the data pool'
-      }
+      })
+      return
     }
     select(doc, "//*[local-name() = 'Receiver']/*[local-name() = 'Identifier']")[0].firstChild.data = dataRecipient
 
@@ -154,14 +156,11 @@ module.exports = function Gdsn(opts) {
     var newId = 'cin_' + Date.now() + '_' + dataRecipient
     select(doc, "//*[local-name() = 'InstanceIdentifier']")[0].firstChild.data = newId
     select(doc, "//*[local-name() = 'message']/entityIdentification/uniqueCreatorIdentification")[0].firstChild.data = newId
-    select(doc, "//*[local-name() = 'message']/entityIdentification/contentOwner/gln")[0].firstChild.data = homeDataPoolGln
+    select(doc, "//*[local-name() = 'message']/entityIdentification/contentOwner/gln")[0].firstChild.data = opts.homeDataPoolGln
 
     // generate new CIN xml
-    var modXml = new xmldom.XMLSerializer().serializeToString(doc)
-    //return modXml
+    var modXml = this.xmldom_serializer.serializeToString(doc)
     
-    if (cb) cb(null, modXml) // for async
-    else return modXml       // for sync usage
+    cb(null, modXml)
   }
-
 }
--- test.js
+++ test.js
@@ -1,37 +1,41 @@
 (function () {
   
   var Gdsn = require('./index.js')
+  var gdsn = new Gdsn()
   
-  var gdsn = new Gdsn({
-    homeDataPoolGln: '1100001011285'
-  })
-  
-  gdsn.readXmlFile('test/cin_from_other_dp.xml', function(err, xml) {
-    if (err) {
-      console.log('Error: ' + err)
-      process.exit(1)
-    }
-    var doc = gdsn.getDocForXml(xml)
-    gdsn.createCinResponse(doc, function(err, responseXml) {
-      var outputFile = 'test/outbox/test_cin_response_' + new Date().getTime() + '.xml'
-      gdsn.writeXmlFile(outputFile, responseXml, function(err, result) {
-        if (err) {
-          console.log('Error: ' + err)
-          process.exit(1)
-        }
-        console.log('Created new CIN response file: ' + outputFile)
-      })
-      gdsn.forwardCinFromOtherDP(doc, function(err, cinOut) {
-        var outputFile = 'test/outbox/test_cin_to_local_party_' + new Date().getTime() + '.xml'
-        gdsn.writeXmlFile(outputFile, cinOut, function(err, result) {
-          if (err) {
-            console.log('Error: ' + err)
-            process.exit(1)
-          }
-          console.log('Created new CIN out file: ' + outputFile)
+  var repeatCount = 10
+  var startTime = new Date().getTime()
+
+  while (repeatCount--) {
+    gdsn.readXmlFile('test/cin_large.xml', function(err, xml) {
+      handleError(err)
+      gdsn.getXmlDom(xml, function (err, doc) {
+        gdsn.createCinResponse(doc, function(err, responseXml) {
+          var outputFile = 'test/outbox/test_cin_response_' + new Date().getTime() + '.xml'
+          gdsn.writeXmlFile(outputFile, responseXml, function(err, result) {
+            handleError(err)
+            console.log('Created new CIN response file: ' + outputFile)
+          })
         })
-      })
-    })
-  })
+        gdsn.forwardCinFromOtherDP(doc, function(err, cinOut) {
+          var outputFile = 'test/outbox/test_cin_to_local_party_' + new Date().getTime() + '.xml'
+          gdsn.writeXmlFile(outputFile, cinOut, function(err, result) {
+            handleError(err)
+            console.log('Created new CIN out file: ' + outputFile)
+          })
+        })    // end forwardCinFromOtherDP
+      })      // end getXmlDom
+    })        // end readXmlFile
+  }
+
+  // time measure not working, logs right away while we wait for async functions
+  console.log('Test run time: ' + ((new Date().getTime() - startTime) / 1000) + ' seconds')
   
 }())
+
+function handleError(err) {
+  if (err) {
+    console.log('Error: ' + err)
+    process.exit(1)
+  }
+}

799076c51a4b508f7e19b31b07de98c0afa4682d
ctoneill <ctoneill@yahoo.com>
2016-02-02 18:55:26 +0000

    started bin 3.1 testing

--- test/test_bin.sh
+++ test/test_bin.sh
@@ -2,17 +2,17 @@
 
 cd `dirname "$0"`
 
-node ../bin/clean_xml.js  cin_from_other_dp.xml
+node ../bin/clean_xml.js  gdsn3/cin_from_other_dp.xml
 
-#node ../bin/cin_forward.js 1100001011285 cin_from_other_dp.xml
+#node ../bin/cin_forward.js 1100001011285 gdsn3/cin_from_other_dp.xml
 
-#node ../bin/cin_respond.js 1100001011285 cin_from_other_dp.xml
+#node ../bin/cin_respond.js 1100001011285 gdsn3/cin_from_other_dp.xml
 
-#node ../bin/extract_trade_items_from_dom.js cin_from_other_dp.xml
+#node ../bin/extract_trade_items_from_dom.js gdsn3/cin_from_other_dp.xml
 
-node ../bin/extract_trade_items_from_stream.js cin_from_other_dp.xml
+node ../bin/extract_trade_items_from_stream.js gdsn3/cin_from_other_dp.xml
 
-node ../bin/extract_parties_from_stream.js rpdd_31_small.xml
+node ../bin/extract_parties_from_stream.js gdsn3/rpdd-gr.xml
 
 node ../bin/validate_gln.js 1100001011285
 
